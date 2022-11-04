e67f0449ecb336e1e36a703458b3b8e6e5531f45
watagori <kotaro.nishigori@gmail.com>
2022-11-05 07:08:12 +0900

    feat: post test message on twitter

---
+++ src/tweet.py
@@ -0,0 +1,17 @@
+import os
+
+import tweepy
+
+consumer_key = os.environ.get("API_KEY")
+consumer_secret = os.environ.get("API_KEY_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret,
+)
+
+client.create_tweet(text="Hello, Watagori!")
