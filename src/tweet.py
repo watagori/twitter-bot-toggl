b5e57d72ab7d1121386f946718bac31304091329
watagori <kotaro.nishigori@gmail.com>
2022-11-07 09:55:16 +0900

    fix: delete print

--- src/tweet.py
+++ src/tweet.py
@@ -14,7 +14,6 @@ TOGGL_API = os.environ.get("TOGGL_API")
 
 def get_toggl_message():
     item_time = {}
-    print(TOGGL_API)
     toggl = Toggl(TOGGL_API)
     workspace_id = toggl.get_workspace_id()
     projects_id = toggl.get_projects_id(workspace_id[0])
